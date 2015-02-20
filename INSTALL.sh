0809372887440cf9782c83d8e60fae42b5aa47ea
George Karakougioumtzis <gkarakou@gmail.com>
2015-02-20 23:10:29 +0200

    Update INSTALL.sh
    added xorg_user to systemd-journal  in INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 if [ -f /etc/pacman.conf ]; then sed -i 's/python/python2/g' systemd-notify.py;fi
+xorg_user=$(w | grep ":0" | cut -d' ' -f1 | sort | uniq)
+usermod -a -G systemd-journal $xorg_user
 cp systemd-notify.py /usr/local/bin
 cp systemd-notify.desktop /etc/xdg/autostart
 chmod 0755 /usr/local/bin/systemd-notify.py
