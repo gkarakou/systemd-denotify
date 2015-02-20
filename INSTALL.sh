f0d03f0fe12803fbd1457e18f2c1bc23cc9efde5
George Karakougioumtzis <gkarakou@gmail.com>
2015-02-20 17:10:21 +0200

    Update INSTALL.sh
    added arch specific command

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+if [ -f /etc/pacman.conf ]; then sed -i 's/python/python2/g' systemd-notify.py
 cp systemd-notify.py /usr/local/bin
 cp systemd-notify.desktop /etc/xdg/autostart
 chmod 0755 /usr/local/bin/systemd-notify.py
