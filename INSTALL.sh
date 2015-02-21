40ffd6624f6c324fe19d09be35ed4be8a8014272
George Karakougioumtzis <gkarakou@gmail.com>
2015-02-21 12:35:27 +0200

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 if [ -f /etc/pacman.conf ]; then sed -i 's/python/python2/g' systemd-notify.py;fi
 xorg_user=$(w | grep ":0" | cut -d' ' -f1 | sort | uniq)
-usermod -a -G systemd-journal $xorg_user
+if [ -z $xorg_user ]
+then
+    echo "either you dont have the w command line utility installed or you are not logged in to your desktop.\n Install w or login to a desktop session and rerun the script";
+    exit
+else
+    usermod -a -G systemd-journal $xorg_user;
+fi
 cp systemd-notify.py /usr/local/bin
 cp systemd-notify.desktop /etc/xdg/autostart
 chmod 0755 /usr/local/bin/systemd-notify.py
