58433e70b15e889f9366131efa9261b1db0d5574
George Karakougioumtzis <gkarakou@gmail.com>
2015-08-11 22:19:10 +0300

    updates on .py

--- systemd-denotify/systemd-denotify.py
+++ systemd-denotify/systemd-denotify.py
@@ -28,55 +28,55 @@ class ConfigReader():
         __init__
         :desc: Constructor function that by returns a ConfigParser object
         """
-        conf = ConfigParser.RawConfigParser()
-        self.conf = conf
-
     #def get_global_entries(self):
 
 
     def get_notification_entries(self):
 
-        self.conf.read('/etc/systemd-denotify.conf')
+        conf = ConfigParser.RawConfigParser()
+        conf.read('/etc/systemd-denotify.conf')
         dictionary = {}
         #parse Logins
-        dictionary['conf_logins_start'] = self.conf.getboolean("Logins", "start")
+        dictionary['conf_logins_start'] = conf.getboolean("Logins", "start")
         #parse FailedServices
-        dictionary['conf_failed_services_start'] = self.conf.getboolean("FailedServices", "start")
+        dictionary['conf_failed_services_start'] = conf.getboolean("FailedServices", "start")
         #parse Files section
-        dictionary['conf_files_start'] = self.conf.getboolean("Files", "start")
-        dictionary['conf_files_dirs'] = self.conf.get("Files", "directories")
+        dictionary['conf_files_start'] = conf.getboolean("Files", "start")
+        dictionary['conf_files_dirs'] = conf.get("Files", "directories")
         dictionary['conf_files_directories'] = dictionary['conf_files_dirs'].split(",")
-        dictionary['conf_files_evs'] = self.conf.get("Files", "events")
+        dictionary['conf_files_evs'] = conf.get("Files", "events")
         dictionary['conf_files_events'] = dictionary['conf_files_evs'].split(",")
         #parse ServicesStatus
-        dictionary['conf_services_start'] = self.conf.getboolean("ServicesStatus", "start")
-        dictionary['conf_services_interval'] = self.conf.getint("ServicesStatus", "interval")
-        dictionary['conf_services_services'] = self.conf.get("ServicesStatus", "services")
+        dictionary['conf_services_start'] = conf.getboolean("ServicesStatus", "start")
+        dictionary['conf_services_interval'] = conf.getint("ServicesStatus", "interval")
+        dictionary['conf_services_services'] = conf.get("ServicesStatus", "services")
         dictionary['conf_services'] = dictionary['conf_services_services'].split(",")
 
         return dictionary
 
     def get_mail_entries(self):
-        self.conf.read('/etc/systemd-denotify.conf')
+        conf = ConfigParser.RawConfigParser()
+        conf.read('/etc/systemd-denotify.conf')
         dictionary = {}
-        dictionary['conf_email_on_user_logins'] = self.conf.getboolean("EMAIL_NOTIFICATIONS", "email_on_user_logins")
-        dictionary['conf_email_on_failed_services'] = self.conf.getboolean("EMAIL_NOTIFICATIONS", "email_on_failed_services")
-        dictionary['conf_email_on_file_alteration'] = self.conf.getboolean("EMAIL_NOTIFICATIONS", "email_on_file_alteration")
-        dictionary['conf_email_on_services_statuses'] = self.conf.getboolean("EMAIL_NOTIFICATIONS", "email_on_services_statuses")
+        dictionary = {}
+        dictionary['conf_email_on_user_logins'] = conf.getboolean("EMAIL_NOTIFICATIONS", "email_on_user_logins")
+        dictionary['conf_email_on_failed_services'] = conf.getboolean("EMAIL_NOTIFICATIONS", "email_on_failed_services")
+        dictionary['conf_email_on_file_alteration'] = conf.getboolean("EMAIL_NOTIFICATIONS", "email_on_file_alteration")
+        dictionary['conf_email_on_services_statuses'] = conf.getboolean("EMAIL_NOTIFICATIONS", "email_on_services_statuses")
 
-        dictionary['conf_email_subject'] = self.conf.get("EMAIL", "subject")
-        dictionary['conf_email_to'] = self.conf.get("EMAIL", "mail_to")
-        dictionary['conf_email_from'] = self.conf.get("EMAIL", "mail_from")
+        dictionary['conf_email_subject'] = conf.get("EMAIL", "subject")
+        dictionary['conf_email_to'] = conf.get("EMAIL", "mail_to")
+        dictionary['conf_email_from'] = conf.get("EMAIL", "mail_from")
 
 
         #parse [AUTH]
 
-        auth = self.conf.getboolean("AUTH", "active")
+        auth = conf.getboolean("AUTH", "active")
         if auth and auth == True:
-            auth_user = self.conf.get("AUTH", "auth_user")
+            auth_user = conf.get("AUTH", "auth_user")
             if len(auth_user) == 0:
                 sys.exit(1)
-            auth_password = self.conf.get("AUTH", "auth_password")
+            auth_password = conf.get("AUTH", "auth_password")
             if len(auth_password) == 0:
                 sys.exit(1)
             dictionary['auth'] = True
@@ -86,55 +86,55 @@ class ConfigReader():
             dictionary['auth'] = False
 
         #parse [SMTP]
-        smtp = self.conf.getboolean("SMTP", "active")
+        smtp = conf.getboolean("SMTP", "active")
         if smtp and smtp == True:
             dictionary['smtp'] = True
         else:
             dictionary['smtp'] = False
-        smtp_host = self.conf.get("SMTP", "host")
+        smtp_host = conf.get("SMTP", "host")
         if len(smtp_host) == 0:
             smtp_host = "localhost"
         dictionary['smtp_host'] = smtp_host
-        smtp_port = self.conf.getint("SMTP", "port")
+        smtp_port = conf.getint("SMTP", "port")
         if not smtp_port:
             smtp_port = 25
         dictionary['smtp_port'] = smtp_port
 
         #parse [SMTPS]
-        smtps = self.conf.getboolean("SMTPS", "active")
+        smtps = conf.getboolean("SMTPS", "active")
         if smtps == True:
             dictionary['smtps'] = True
-            smtps_host = self.conf.get("SMTPS", "host")
+            smtps_host = conf.get("SMTPS", "host")
             if len(smtps_host) == 0:
                 smtps_host = "localhost"
             dictionary['smtps_host'] = smtps_host
-            smtps_port = self.conf.getint("SMTPS", "port")
+            smtps_port = conf.getint("SMTPS", "port")
             if not smtps_port:
                 smtps_port = 465
             dictionary['smtps_port'] = smtps_port
-            smtps_cert = self.conf.get("SMTPS", "cert_file")
+            smtps_cert = conf.get("SMTPS", "cert_file")
             dictionary['smtps_cert'] = smtps_cert
-            smtps_key = self.conf.get("SMTPS", "key_file")
+            smtps_key = conf.get("SMTPS", "key_file")
             dictionary['smtps_key'] = smtps_key
         else:
             dictionary['smtps'] = False
 
         #parse [STARTTLS]
-        starttls = self.conf.getboolean("STARTTLS", "active")
+        starttls = conf.getboolean("STARTTLS", "active")
         if  starttls == True:
             dictionary['starttls'] = True
-            starttls_host = self.conf.get("STARTTLS", "host")
+            starttls_host = conf.get("STARTTLS", "host")
             if len(starttls_host) == 0:
                 starttls_host = "localhost"
 
             dictionary['starttls_host'] = starttls_host
-            starttls_port = self.conf.getint("STARTTLS", "port")
+            starttls_port = conf.getint("STARTTLS", "port")
             if not starttls_port:
                 starttls_port = 587
             dictionary['starttls_port'] = starttls_port
-            starttls_cert = self.conf.get("STARTTLS", "cert_file")
+            starttls_cert = conf.get("STARTTLS", "cert_file")
             dictionary['starttls_cert'] = starttls_cert
-            starttls_key = self.conf.get("STARTTLS", "key_file")
+            starttls_key = conf.get("STARTTLS", "key_file")
             dictionary['starttls_key'] = starttls_key
         else:
             dictionary['starttls'] = False
@@ -646,11 +646,11 @@ class FileNotifier():
         c_read = ConfigReader()
         dictio = c_read.get_notification_entries()
         #dictioo = c_read.get_mail_entries()
-        mappings = {WRITE:pyinotify.IN_CLOSE_WRITE, MODIFY:pyinotify.IN_MODIFY, DELETE:pyinotify.IN_DELETE, ATTRIBUTE:pyinotify.IN_ATTRIB}
+        mappings = {"WRITE":pyinotify.IN_CLOSE_WRITE, "MODIFY":pyinotify.IN_MODIFY, "DELETE":pyinotify.IN_DELETE, "ATTRIBUTE":pyinotify.IN_ATTRIB}
         mask = ""
         for k, v in dictio['conf_files_events']:
             for key, value in mappings:
-                if v == key:
+                if string(v) == key:
                     mask += value +" | "
         #debug
                     if k == len(dictio['conf_file_events']) -1:
