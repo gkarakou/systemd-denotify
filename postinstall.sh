2c970bdc449ea0bdb9de22058461d97bec7aa78e
George Karakougioumtzis <gkarakou@gmail.com>
2017-04-23 18:06:01 +0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -5,11 +5,9 @@ getXorgUser=`/usr/bin/ps -aux|/usr/bin/grep Xorg|/usr/bin/cut -d " " -f1|/usr/bi
 getXwaylandUser=`/usr/bin/ps -aux|/usr/bin/grep "Xwayland :0"|/usr/bin/cut -d " " -f1|/usr/bin/sort|/usr/bin/uniq`
 if [ ! -z "$getXuser" ];then
 /usr/sbin/usermod -a -G systemd-journal $getXuser
-fi
-if [ ! -z "$getXorgUser" ];then
+elif [ ! -z "$getXorgUser" ];then
 /usr/sbin/usermod -a -G systemd-journal $getXorgUser
-fi
-if [ ! -z "$getXwaylandUser" ];then
+else [ ! -z "$getXwaylandUser" ];then
 /usr/sbin/usermod -a -G systemd-journal $getXwaylandUser
 fi
 exit 0;
