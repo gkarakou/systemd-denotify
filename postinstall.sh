1769030e4a2a482ccc7f99eae9e0c504a697639f
George Karakougioumtzis <gkarakou@gmail.com>
2017-04-23 18:04:20 +0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 getXuser=`/usr/bin/w |grep :0|cut -d " " -f1|sort|uniq`
+#fedora
+getXorgUser=`/usr/bin/ps -aux|/usr/bin/grep Xorg|/usr/bin/cut -d " " -f1|/usr/bin/sort|/usr/bin/uniq`
+getXwaylandUser=`/usr/bin/ps -aux|/usr/bin/grep "Xwayland :0"|/usr/bin/cut -d " " -f1|/usr/bin/sort|/usr/bin/uniq`
 if [ ! -z "$getXuser" ];then
-/sbin/usermod -a -G systemd-journal $getXuser
+/usr/sbin/usermod -a -G systemd-journal $getXuser
+fi
+if [ ! -z "$getXorgUser" ];then
+/usr/sbin/usermod -a -G systemd-journal $getXorgUser
+fi
+if [ ! -z "$getXwaylandUser" ];then
+/usr/sbin/usermod -a -G systemd-journal $getXwaylandUser
 fi
 exit 0;
