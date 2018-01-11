3578f1bfc6b9ad01c8ef91c447f76a50b462e357
george Karakougioumtzis <gkarakou@gmail.com>
2018-01-11 16:52:47 +0200

    removed for else

--- denotify/journalparser.py
+++ denotify/journalparser.py
@@ -106,10 +106,6 @@ class JournalParser(threading.Thread):
                                         pass
                                 else:
                                     pass
-                            #python for ... else exclusive loop statement
-                            else:
-                                break
-                            #continue
                         except Exception as ex:
                             template = "An exception of type {0} occured. Arguments:\n{1!r}"
                             message = template.format(type(ex).__name__, ex.args)
